379e4479867dfe9c8ce926380c2489b7f4081fe8
Katsuhiro Sasaki <k2hiro@gmail.com>
2025-12-10 00:10:32 +0800

    fix file

--- peak.py
+++ peak.py
@@ -2,54 +2,39 @@
 #
 #   peak.py
 #
-#   指定されたカウンタの min, max, avg を表示
+#   指定されたパラメータの min, max, avg を表示
 #
 
-
+import sys
 import re
-import argparse
 
-if __name__ == '__main__':
+import fileinput
 
-    parser = argparse.ArgumentParser(
-        description=f"Calculate retry rates in 'show ap debug radio-stats'")
-    parser.add_argument('infile', help="Input files(s) containing 'show ap debug radio-stats' output", type=str, nargs='+')
-    args = parser.parse_args()
+if len(sys.argv) < 2:
+    print("Usage: peak.py <param> <input-fil(s)>")
+    sys.exit(1)
 
-    #
-    #   parse radio stats
-    #
-    for fn in args.infile:
-        try:
-            f = open(fn, encoding='utf-8')
-            lines = f.readlines()
-        except UnicodeDecodeError as e:
-            f = open(fn, encoding='mac_roman')
-            lines = f.readlines()
-        f.close()
-        pre_txtime = 0
-        pre_txbcmc = 0
-        txtime = None
-        txbcmc = None
-        for l in lines:
-            if l.startswith('Tx Time Data Transmitted'):
-                m = re.search(r'\d+', l)
-                txtime = int(m.group(0)) if m else None
-                continue
-            if l.startswith('Tx Time BC/MC Data'):
-                m = re.search(r'\d+', l)
-                txbcmc = int(m.group(0)) if m else None
+p = sys.argv[1]
+for fn in sys.argv[2:]:
+    min_val = 0xFFFFFFFF
+    max_val = 0
+    sum_val = 0
+    num = 0
+    with open(fn, encoding='utf-8') as f:
+        lines = f.readlines()
 
-                if txbcmc is not None and txtime is not None and txbcmc > 0:
-                    if pre_txtime == 0:
-                        pre_txtime = txtime
-                        pre_txbcmc = txbcmc
-                        continue
-                    d_txtime = txtime - pre_txtime
-                    d_txbcmc = txbcmc - pre_txbcmc
-                    pre_txtime = txtime
-                    pre_txbcmc = txbcmc
-                    bcmc_rate = (d_txbcmc/ d_txtime) * 100
-                    print(f"{fn} {bcmc_rate:.2f}% ({d_txbcmc}/{d_txtime})")
+    for l in lines:
+        if p not in l:
+            continue
+        m = re.search(r'\d+$', l)
+        if not m:
+            continue
 
-                continue
+        num += 1
+        val = int(m.group(0))
+        sum_val += val
+        min_val = min(min_val, val)
+        max_val = max(max_val, val)
+
+    print(f'{fn:<25}: min={min_val:<5} max={max_val:<5} avg={sum_val / num if num > 0 else 0}')
+
